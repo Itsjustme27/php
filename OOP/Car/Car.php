f081a40ab6a9134ce77c8a9ad4aa094afeb73cb9
Itsjustme27 <reachprayush@gmail.com>
2024-12-23 08:30:15 +0545

    OOP

---
+++ OOP/Car/Car.php
@@ -0,0 +1,43 @@
+<?php
+    class Car {
+        public $name;
+        public $price;
+
+        public function __construct($name, $price) {
+            $this->name = $name;
+            $this->price = $price;
+        }
+
+        public function getName() {
+            return $this->name;
+        }
+
+        public function getPrice() {
+            return $this->price;
+        }
+
+        // public function setName($name) {
+        //     $this->name = $name;
+        // }
+
+        // public function setPrice($price) {
+        //     $this->price = $price;
+        // }
+
+    }
+
+    $toyota = new Car("Toyota", 5000000);
+    // $toyota->setName("Toyota");
+    // $toyota->setPrice(50055050);
+
+    $carName = $toyota->getName();
+    $carPrice = $toyota->getPrice();
+
+    echo "Car name: $carName \n";
+    echo "Car price: $carPrice \n";
+    // $toyota->name = "Toyota";
+    // $toyota->price = 1000000;
+
+    // echo "Car name: $toyota->name \n";
+    // echo "Car price: $toyota->price \n";
+?>
