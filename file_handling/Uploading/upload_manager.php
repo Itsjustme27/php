ef35ec44d9400776392e953166189d1851abfb56
Itsjustme27 <reachprayush@gmail.com>
2025-03-27 12:53:56 +0545

    file handling with exception

--- file_handling/Uploading/upload_manager.php
+++ file_handling/Uploading/upload_manager.php
@@ -1,36 +1,36 @@
 <?php
-$uploadDir = 'uploads/';
-
-$allowedTypes = ['jpg', 'jpeg', 'png', 'txt'];
+$fileDir = 'uploads/';
+$uploadTypes = ['jpg', 'png', 'jpeg', 'txt'];
 $maxFileSize = 2 * 1024 * 1024;
 
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-	$file = $_FILES['file'];
+try{
+	if($_SERVER['REQUEST_METHOD'] == 'POST') {
+		$file = $_FILES['file'];
+		
+		// Error handling
+		if($file['error'] !== UPLOAD_ERR_OK) {
+			throw new Exception("File upload error: " . $file['error']);
+		}
+		
+		// validating file size
+		if($file['size'] > $maxFileSize) {
+			throw new Exception("MAX FILE SIZE REACHED: 2MB ONLY");
+		}
+		
+		// extracting file extension
+		$fileExtension = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
 
-	print_r($_FILES['file']);
-
-	if($file['error'] !== UPLOAD_ERR_OK) {
-		die("File upload error: " . $file['error']);
+		if(!in_array($fileExtension, $uploadTypes)) {
+			throw new Exception('Invalid File type: png, jpg, jpeg, txt');
+		}
+		
+		// creating a safe name
+		$safeFileName = uniqid('upload_', true) . '.' . $fileExtension;
+		if(!move_uploaded_file($file['tmp_name'], $fileDir . $safeFileName)) {
+			throw new Exception('File not moved');
+		}
 	}
-
-	$fileExtension = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
-	if(!in_array($fileExtension, $allowedTypes)) {
-		die("Invalid Type");
-	}
-
-
-	if($file['size'] > $maxFileSize) {
-		die("File too large. Max 2MB allowed");
-	}
-
-	$safeFileName = uniqid('upload_', true) . '.' . $fileExtension;
-
-	if(!move_uploaded_file($file['tmp_name'], $uploadDir . $safeFileName)) {
-		die("Error moving uploaded file");
-	} 
-
-	echo "File uploaded successfully: " . htmlspecialchars($safeFileName);	
-} else {
-	echo "No file uploaded";
+}catch(Exception $e){
+	throw new Exception('Exception: ' . $e->getMessage());
 }
 ?>
